b84b10e0100cfe9975889f9202437eb5810b7453
Andr3i04 <amoldovan710@gmail.com>
2025-03-25 08:52:39 +0200

    lab5

--- src/Persoana.java
+++ src/Persoana.java
@@ -19,8 +19,28 @@ public class Persoana implements Comparable<Persoana>{
         return nume + " " + prenume + " " + varsta;
     }
 
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
     public String getPrenume() {
         return prenume;
     }
 
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public int getVarsta() {
+        return varsta;
+    }
+
+    public void setVarsta(int varsta) {
+        this.varsta = varsta;
+    }
 }
+
